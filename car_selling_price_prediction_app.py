9a9e2685e8e9a8eabff60a259ab8e92f21ec8308
Kundan Yadav <112746678+kundanyadavGithub@users.noreply.github.com>
2023-05-22 21:16:13 +0530

    Update car_selling_price_prediction_app.py

--- car_selling_price_prediction_app.py
+++ car_selling_price_prediction_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 from sklearn import *
 import pickle 
 import streamlit as st
